4f1629c62769dd30c8e0e67d45d9ab926538e8f9
BanceChattles <cb0885404@otc.edu>
2019-03-27 13:16:05 -0500

    Pseudo Calender
    Changed location of one piece of code

--- javascript.js
+++ javascript.js
@@ -9,9 +9,10 @@ var months = [
 ];
 var days = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 for (i = 0; i< months.length; i++)
-{console.log(moment().month(i).format("MMMM"))};
+{console.log(moment().month(i).format("MMMM"))
+console.log(_.chunk(days, 7));};
 
 
 
-console.log(_.chunk(days, 7));
-console.log(moment().date(1).format('DD'));
+
+//console.log(moment().date(1).format('DD'));
