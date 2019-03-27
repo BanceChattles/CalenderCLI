56b3ea0f331795db7d3f2d2884de822e0de53da0
BanceChattles <cb0885404@otc.edu>
2019-03-27 13:14:36 -0500

    Fixed Packages
    Testing with moment and lodash

---
+++ javascript.js
@@ -0,0 +1,17 @@
+var _ = require('lodash');
+var moment = require('moment');
+const MomentRange = require('moment-range');
+const chalk = require('chalk');
+
+var months = [
+    "January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December"
+];
+var days = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+for (i = 0; i< months.length; i++)
+{console.log(moment().month(i).format("MMMM"))};
+
+
+
+console.log(_.chunk(days, 7));
+console.log(moment().date(1).format('DD'));
