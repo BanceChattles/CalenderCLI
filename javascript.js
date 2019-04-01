7a36b3946df347c92cd3f2ec8ba8a72862bce2f8
BanceChattles <cb0885404@otc.edu>
2019-04-01 11:21:42 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -10,7 +10,7 @@ var month = [
 var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 for (i = 0; i< month.length; i++)
-{console.log(chalk.blue(moment().month(i).format("MMMM")))
+{console.log(chalk.blue((moment().month(i).format("MMMM"))))
 console.log(_.pad(day, 8))
 console.log(_.chunk(days, 7));};
 
